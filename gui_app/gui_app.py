af1421e916c94d955eb6a538178ddbd711604fd3
samwie <wierzbickisamuel@gmail.com>
2023-11-14 19:36:57 +0100

    Develop gui app

---
+++ gui_app/gui_app.py
@@ -0,0 +1,73 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+from tkinter import filedialog
+import torch
+import cv2
+import numpy as np
+from skimage.color import rgb2lab, lab2rgb
+from torchvision import transforms
+import numpy as np
+
+import sys
+
+sys.path.append(
+    "/home/samuel/Dokumenty/Programy/Grayscale-image-coloring-application/model_training_section"
+)
+from model_structure import UNet
+
+root = Tk()
+
+root.title("Image coloring application")
+root.geometry("600x400")
+
+model = UNet()
+model.load_state_dict(
+    torch.load(
+        "/home/samuel/Dokumenty/Programy/Grayscale-image-coloring-application/model_training_section/trained_model.pth",
+        map_location=torch.device("cpu"),
+    )
+)
+
+
+global image
+
+
+def select_path():
+    global tk_image
+    global image
+
+    path = filedialog.askopenfilename()
+    image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    # image = cv2.resize(image, (256, 256))
+    image_pil = Image.fromarray(image)
+
+    im_window = ttk.Frame(root, padding=10)
+    im_window.place(relx=0, rely=0.1)
+    tk_image = ImageTk.PhotoImage(image_pil)
+    ttk.Label(im_window, image=tk_image).grid(column=0, row=0)
+
+
+def predict():
+    global image
+    global tk_image_pred
+
+    img_normalized = image / 50.0 - 1
+    img_tensor = torch.tensor(img_normalized).float().unsqueeze(0).unsqueeze(0)
+    image_pred = model.predict(img_tensor)
+    tk_image_pred = ImageTk.PhotoImage(image_pred)
+    im_window = ttk.Frame(root, padding=10)
+
+    im_window.place(relx=0.5, rely=0.1)
+    ttk.Label(im_window, image=tk_image_pred).grid(column=0, row=0)
+
+
+# buttons
+btn_1 = Button(root, text="Load image", fg="black", command=select_path)
+btn_1.place(relx=0.2, rely=0.01)
+
+btn_2 = Button(root, text="Predict", fg="black", command=predict)
+btn_2.place(relx=0.7, rely=0.01)
+
+
+root.mainloop()
---
+++ model_training_section/make_dataloaders.py
@@ -0,0 +1,38 @@
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+import numpy as np
+from skimage.color import rgb2lab
+
+PIC_SIZE = 256
+
+
+class ColorizationDataset(Dataset):
+    def __init__(self, paths):
+        self.transforms = transforms.Resize((PIC_SIZE, PIC_SIZE), Image.BICUBIC)
+
+        self.pic_size = PIC_SIZE
+        self.paths = paths
+
+    def __getitem__(self, idx):
+        img = Image.open(self.paths[idx]).convert("RGB")
+        img = self.transforms(img)
+        img = np.array(img)
+        img_lab = rgb2lab(img).astype("float32")
+        img_lab = transforms.ToTensor()(img_lab)
+        L = img_lab[[0], ...] / 50.0 - 1.0
+        ab = img_lab[[1, 2], ...] / 110.0
+
+        return {"L": L, "ab": ab}
+
+    def __len__(self):
+        return len(self.paths)
+
+
+def make_dataloaders(batch_size=16, n_workers=1, pin_memory=True, **kwargs):
+    dataset = ColorizationDataset(**kwargs)
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, num_workers=n_workers, pin_memory=pin_memory
+    )
+
+    return dataloader
